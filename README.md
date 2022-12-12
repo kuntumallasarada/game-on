8146372034fd144850b682f87cd81f5b660fb15b
Sarada Nookala <kuntumallasarada@gmail.com>
2022-12-12 00:41:24 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # game-on
+![My Image](C:\Users\kuntu\Downloads\IMG_3537.jpg)
+Technologies Used: HTML, CSS and JavaScript.
+DOM is used to see the results.
+Steps took to complete the project:
+1. Downloaded all the immages needed.
+2. A button is created to start the game - Rolling the dice function.
+3. Memory board is created .
+4. Depending on the result of dice the player or computer will start the game.
+5. Individual functions to flip the cards selected by player and computer were written.
+6. Individual functions to match the cards selected by the player and computer were written.
+7. The matched cards were kept in their respective arrays and they were compared to select the winner.
+8. A reset button is placed to restart the game.
+
+Challenges:
+1. Math.Random method is used to select the memory cards for computer. It doesn't always generate the random numbers. It generates the same number twice if called again. This feature gave hard time in writing the code for selecting the computer's cards.
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ let player2 = new player("Computer",0)
 let diceCount=0;
 // Roll a dice function
 const result=document.querySelector('.diceresult');
+
 function rollDice(){
     player1.dice = Math.floor(Math.random()*6)+1;
     player2.dice = Math.floor(Math.random()*6)+1;
@@ -27,10 +28,6 @@ function rollDice(){
     }
     
 }
-
-
-//document.addEventListener('DOMContentLoaded',() => {
-
     //memory cards
     const cardArray = [
      {
@@ -82,9 +79,9 @@ function rollDice(){
          img: 'images/vidia.png'
      }
   ]
+
   //shuffling the card array
   // cardArray.sort(() => 0.5 - Math.random())
-
 for (let i = cardArray.length -1; i > 0; i--) {
   let j = Math.floor(Math.random() * (i+1));
   let k = cardArray[i];
@@ -99,6 +96,13 @@ for (let i = cardArray.length -1; i > 0; i--) {
   computerChosenId = [];
   cardsWonPlayer = [];
   cardsWonComp = [];
+  cardsWonAll = [];
+  choiceArray = [];
+  let playerResult=document.querySelector('.playerScore');
+  playerResult.textContent = 0;
+  let compResult=document.querySelector('.computerScore');
+  compResult.textContent = 0;
+  let endResult = document.querySelector('.gameresult');
 
   //create your board
   function createBoard(){
@@ -108,86 +112,146 @@ for (let i = cardArray.length -1; i > 0; i--) {
          card.setAttribute('data-id',i);
          card.id =i
          container.appendChild(card);    
-     
-     if(diceCount==1){
+      if(diceCount==1){
         card.addEventListener('click',flipCard);
         }
          if(diceCount==2 && i==11){
-             flipcardComp();
+             setTimeout(flipcardComp,1500);
           }
         }
   }
 
-  //flipCard function
+  //flipCard function for Player
 
   function flipCard() {
+    console.log("flipcardplayer");
     let cardId = this.getAttribute('data-id');
     cardsChosen.push(cardArray[cardId].name);
     cardsChosenId.push(cardId);
     this.setAttribute('src',cardArray[cardId].img);
+   
     if(cardsChosen.length == 2){
         setTimeout(checkForMatch, 1500);    
     }
   } 
 
-  //Check for Matches
+  //Matching the player's cards
   function checkForMatch(){
+    console.log("checkForMatchplayer")
     let cards = document.querySelectorAll('img');
     let optionOneId = cardsChosenId[0];
     let optionTwoId = cardsChosenId[1];
-    let one = parseInt(optionOneId)+1;
-    let two = parseInt(optionTwoId)+1;
+    let one = parseInt(optionOneId)+2;
+    let two = parseInt(optionTwoId)+2;
 
     if(cardsChosen[0]===cardsChosen[1]){
-       cardsWonPlayer.push(cardsChosen);
+       cardsWonPlayer.push(cardsChosen[0]);
+       cardsWonAll.push(cardsChosen[0]);
+       result.textContent="Found a match, find more!";
+       for(let i=0;i<cards.length;i++){
+       cards[i].addEventListener('click',flipCard);
+       playerResult.textContent = cardsWonPlayer.length;
+       }
     }
     else {      
          cards[one].setAttribute('src','images/fairy.png');
          cards[two].setAttribute('src','images/fairy.png');
-         alert("It's computer's turn to play");
+         result.textContent="Missed!It's Computer's turn to play now!";
+         setTimeout(flipcardComp,1500);
     }
     cardsChosen = [];
     cardsChosenId = [];
-    console.log(cardsWonPlayer)
+    if(cardsWonAll.length == cardArray.length/2){
+         if(cardsWonPlayer.length>cardsWonComp.length)
+         endResult.textContent = "Player is the winner !!!";
+         else 
+         endResult.textContent = "Computer is the winner!!!";
+    }
+   
   }
+  
+  //Computer's functions
+     //Generating unique choice numbers
   function compCards(){
-        const choiceNumber = Math.floor(Math.random()*12);
+        let choiceNumber = Math.floor(Math.random()*12);
+         if(choiceArray.length>0){
+            let value = choiceArray.includes(choiceNumber);
+            if(value===true){
+                choiceNumber = Math.floor(Math.random()*12); 
+         }
+        }
+        choiceArray.push(choiceNumber);
+        if(cardsWonAll.length>0){
+            for(let i=0;i<cardsWonAll.length*2;i++){
+              let value1 = cardsWonAll.includes(cardArray[choiceNumber].name)
+            if(value1===true){
+                choiceNumber = Math.floor(Math.random()*12); 
+                let value2 = choiceArray.includes(choiceNumber);
+                if(value2 === true){
+                    choiceNumber = Math.floor(Math.random()*12); 
+             }                
+            }
+            choiceArray.push(choiceNumber);
+        }
+        }
         let  cardId = document.getElementById(choiceNumber.toString());
          computerChosen.push(cardArray[choiceNumber].name);
         computerChosenId.push(choiceNumber);
        cardId.src = cardArray[choiceNumber].img
+        
   }
 
-  //Computer playing
+
+  //Computer's flipcard function
   function flipcardComp(){
       for(i=0;i<2;i++){
             compCards();
       }
+      choiceArray = [];
   setTimeout(checkForMatchComp,2000);
 }
 
-  //Check for Matches --- Computer Playing
+  //Function to match Computer's cards
   function checkForMatchComp(){
-    let cards = document.querySelectorAll('img');
+      let cards = document.querySelectorAll('img');
     let optionOneId = computerChosenId[0];
     let optionTwoId = computerChosenId[1];
-    let one = parseInt(optionOneId)+1;
-    let two = parseInt(optionTwoId)+1;
+    let one = parseInt(optionOneId)+2;
+    let two = parseInt(optionTwoId)+2;
   
     if(computerChosen[0]===computerChosen[1]){
         cards[one].setAttribute('src',cardArray[optionOneId].img);
         cards[two].setAttribute('src',cardArray[optionTwoId].img);
-        cardsWonComp.push(computerChosen);
+        cardsWonComp.push(computerChosen[0]);
+        cardsWonAll.push(computerChosen[0]);
+        result.textContent="Found a match, find more Computer!";
+    compResult.textContent = cardsWonComp.length;
+        setTimeout(flipcardComp,1500);
     }
     else {   
       
          cards[one].setAttribute('src','images/fairy.png');
          cards[two].setAttribute('src','images/fairy.png');
+         result.textContent="Missed!It's Player's turn to play now!";
+         for(let i=0;i<cards.length;i++){
+            cards[i].addEventListener('click',flipCard);
+            }
+         
     }
     computerChosen = [];
     computerChosenId = [];
+   if(cardsWonAll.length == cardArray.length/2){
+    if(cardsWonPlayer.length>cardsWonComp.length)
+    endResult.textContent = "Player is the winner !!!";
+    else 
+    endResult.textContent = "Computer is the winner!!!";
+}
   }
 
- 
- 
-//  })
+  const reloadBtn = document.querySelector('.reload');
+  reloadBtn.innerText = 'Restart';
+  reloadBtn.style.display = 'flex'
+  reloadBtn.addEventListener('click',() => {
+      window.location.reload();
+  })
+
